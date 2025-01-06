df56ee773ac7b3d3c5225d54aaeacdd68409c449
barbiehu <hugo.barbieri@etu.univ-grenoble-alpes.fr>
2025-01-06 16:35:20 +0100

    chore: update popover components

--- components/popover/Pictures.tsx
+++ components/popover/Pictures.tsx
@@ -8,17 +8,17 @@ export default function Pictures({ handleClickActive }: MenuProps) {
       <main className=" absolute top-0 z-40 flex w-[100vw] h-[100vh]">
         <section className=" flex flex-col items-center self-center gap-12 w-[75vw] h-[95vh] bg-white bg-opacity-60 rounded-3xl backdrop-blur-md mx-auto px-7 py-12 overflow-y-scroll scrollbarhidden">
           <button
-            className="absolute top-5 left-5"
+            className="absolute top-5 left-5 transform transition-transform duration-300 hover:-rotate-90"
             onClick={() => handleClickActive("home")}
           >
             <img
-              src="/icones/arrow-back.svg"
+              src="/icones/xmark-solid.svg"
               alt="arrow-back"
               width={24}
               height={24}
             />
           </button>
-          <h1 className=" font-gravitas self-center text-7xl">
+          <h1 className=" font-gravitas self-center mt-4 text-7xl">
             Importer une image
           </h1>
           <div className=" my-auto">
