027782442df057b44b1dced80c67b272ab8ae99a
bastien <bastien.allegre@etu.univ-grenoble-alpes.fr>
2025-01-06 15:57:31 +0100

    fix : modification pour que les fonction de image soit asyncrone

--- components/model/Image.tsx
+++ components/model/Image.tsx
@@ -29,41 +29,53 @@ class Image extends Component<ImageProps, ImageState> {
     };
   }
 
-  create = (id: number, name: string, url: string) => {
+  async create(id: number, name: string, url: string) {
     try {
-      const reponce = supabaseData
+      const reponce = await supabaseData
         .from("Image")
         .insert({ id: id, name: name, url: url });
     } catch (e) {
       console.log(e);
     }
-  };
+  }
 
-  read = (id: number) => {
+  async read(id: number) {
     try {
-      const reponce = supabaseData.from("Image").select().eq("id", id);
+      const { data, error } = await supabaseData
+        .from("Image")
+        .select()
+        .eq("id", id);
+      if (error) {
+        throw error;
+      }
+      const initimage = {
+        id: data?.[0]?.id,
+        name: data?.[0]?.name,
+        url: data?.[0]?.url,
+      };
+      return initimage;
     } catch (e) {
       console.log(e);
     }
-  };
+  }
 
-  update = (id: number, name: string, url: string) => {
+  async pdate(id: number, name: string, url: string) {
     try {
-      const reponce = supabaseData
+      const reponce = await supabaseData
         .from("Image")
         .update({ id: id, name: name, url: url });
     } catch (e) {
       console.log(e);
     }
-  };
+  }
 
-  deleteentrée = (id: number) => {
+  async deleteentrée(id: number) {
     try {
-      const reponce = supabaseData.from("Image").delete().eq("id", id);
+      const reponce = await supabaseData.from("Image").delete().eq("id", id);
     } catch (e) {
       console.log(e);
     }
-  };
+  }
 
   // Méthode pour supprimer un utilisateur
   delete(): void {
