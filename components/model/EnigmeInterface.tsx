2581dcf9221dcb08b1a671d60047b4bcfd0cd9cc
Nereol <pimboliwell.fr@gmail.com>
2025-01-09 09:11:59 +0100

    ajout des triggerbox pour les enigmes

---
+++ components/model/EnigmeInterface.tsx
@@ -0,0 +1,112 @@
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+import { get } from "http";
+
+export type UseEnigme = {
+    EnigmeData: EnigmeData;
+    getidPhare: () => number;
+    getidEnigme: () => number;
+    getcodeLock: () => number;
+    getcoordX: () => number;
+    getcoordY: () => number;
+    getlenghtX: () => number;
+    getlenghtY: () => number;
+    getname: () => string;
+    getpositionLock: () => number;
+    getquestion: () => string;
+    gettext1: () => string;
+    getanswer: () => string;
+    gettext2: () => string;
+};
+
+const supabaseData = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
+);
+
+interface EnigmeData {
+  idPhare: number;
+  idEnigme: number;
+  codeLock: number,
+  coordX: number,
+  coordY: number,
+  lenghtX: number,
+  lenghtY: number,
+  name: string,
+  question: string,
+  text1: string,
+  answer: string,
+  text2: string
+}
+
+export function useEnigme(idPhare: number, idEnigme: number) {
+  const [EnigmeData, setEnigmeData] = useState<EnigmeData>({
+    idPhare: 0,
+    idEnigme: 0,
+    codeLock: 2,
+    coordX: 0,
+    coordY: 0,
+    lenghtX: 10,
+    lenghtY: 10,
+    name: "",
+    question: "",
+    text1: "",
+    answer: "",
+    text2: ""
+  });
+
+  useEffect(() => {
+    const fetchEnigmeData = async () => {
+      try {
+        if (idPhare >= 1) {
+          const request = await supabaseData
+            .from("Enigme")
+            .select()
+            .eq("idPhare", idPhare)
+            .eq("idEnigme", idEnigme)
+            .single();
+          if (request.data) {
+            //console.log("Requete Enigme : ", request.data);
+            setEnigmeData({
+                idPhare: idPhare,
+                idEnigme: idEnigme,
+                codeLock: request.data.codeLock,
+                coordX: request.data.coordX,
+                coordY: request.data.coordY,
+                lenghtX: request.data.lenghtX,
+                lenghtY: request.data.lenghtY,
+                name: request.data.name,
+                question: request.data.question,
+                text1: request.data.text1,
+                answer: request.data.answer,
+                text2: request.data.text2
+            });
+
+          }
+        }
+      } catch (e) {
+        console.log("on entre dans l'erreur");
+        console.error("Erreur lors de la récupération des données");
+      }
+    };
+    fetchEnigmeData();
+  }, [idPhare, idEnigme]);
+
+  //methodes
+  const methods = {
+    getidPhare: () => EnigmeData.idPhare,
+    getidEnigme: () => EnigmeData.idEnigme,
+    getanswerLock: () => EnigmeData.codeLock,
+    getcoordX: () => EnigmeData.coordX,
+    getcoordY: () => EnigmeData.coordY,
+    getlenghtX: () => EnigmeData.lenghtX,
+    getlenghtY: () => EnigmeData.lenghtY,
+    getname: () => EnigmeData.name,
+    getquestion: () => EnigmeData.question,
+    gettext1: () => EnigmeData.text1,
+    getanswer: () => EnigmeData.answer,
+    gettext2: () => EnigmeData.text2
+  };
+
+  return { EnigmeData, ...methods };
+}
--- components/popover/Enigme.tsx
+++ components/popover/Enigme.tsx
@@ -1,9 +1,17 @@
+import { useEnigme, UseEnigme } from "@/components/model/EnigmeInterface";
+
 interface EnigmeProps {
     handleClickActive: (a: string) => void;
     id: number;
   }
-  
   export default function Enigme({ handleClickActive,id }: EnigmeProps) {
+
+    const enigme1 = useEnigme(id, 1);//recupere les données de la premiere enigme du phare id
+    const enigme2 = useEnigme(id, 2);//recupere les données de la deucieme enigme du phare id
+    const enigme3 = useEnigme(id, 3);//recupere les données de la troisieme enigme du phare id
+    const enigme4 = useEnigme(id, 4);//recupere les données de la quatrieme enigme du phare id
+
+    console.log("enigme1",enigme1.getcoordX(),enigme1.getcoordY(),enigme1.getlenghtX(),enigme1.getlenghtY());
     return (
       <>
         <main className=" absolute top-0 z-40 flex w-[100vw] h-[100vh]">
@@ -20,10 +28,15 @@ interface EnigmeProps {
               />
             </button>
             <div className="rounded-3xl absolute top-1/2 -translate-y-1/2 left-[10%] w-[40vw] h-[40vw] bg-cover bg-center" style={{ backgroundImage: `url("https://nereoll.github.io/imagesPhare/phares/${id}.png")` }}>
-            <button className="absolute top-[20vw] left-[20vw] w-[2vw] h-[2vw] hover:border-red-700 border-2 rounded-lg" onClick={() => handleClickActive("enigme")}>
+            <button style={{left: `${enigme1.getcoordX()}vw`, top:`${enigme1.getcoordY()}vw`, width : `${enigme1.getlenghtX()}vw`, height : `${enigme1.getlenghtY()}vw`}} className={`absolute border-red-700 border-2 rounded-lg`}  onClick={() => handleClickActive("enigme")}>
+            </button>
+            <button style={{left: `${enigme2.getcoordX()}vw`, top:`${enigme2.getcoordY()}vw`, width : `${enigme2.getlenghtX()}vw`, height : `${enigme2.getlenghtY()}vw`}} className={`absolute border-red-700 border-2 rounded-lg`}  onClick={() => handleClickActive("enigme")}>
+            </button>
+            <button style={{left: `${enigme3.getcoordX()}vw`, top:`${enigme3.getcoordY()}vw`, width : `${enigme3.getlenghtX()}vw`, height : `${enigme3.getlenghtY()}vw`}} className={`absolute border-red-700 border-2 rounded-lg`}  onClick={() => handleClickActive("enigme")}>
+            </button>
+            <button style={{left: `${enigme4.getcoordX()}vw`, top:`${enigme4.getcoordY()}vw`, width : `${enigme4.getlenghtX()}vw`, height : `${enigme4.getlenghtY()}vw`}} className={`absolute border-red-700 border-2 rounded-lg`}  onClick={() => handleClickActive("enigme")}>
             </button>
             </div>
-
           </section>
         </main>
       </>
