871d0a6ff92cedfd6dfd91f347705c153211acc9
bastien <bastien.allegre@etu.univ-grenoble-alpes.fr>
2025-01-10 10:40:02 +0100

    fix : merge conflict

--- components/OneBeacoin.tsx
+++ components/OneBeacoin.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { useBeacoin } from "./model/Beacoin";
-import { useImage } from "./model/Image";
+import Link from "next/link";
 
 interface OnBeacoinProp {
   id_beacoin: number;
@@ -17,7 +17,10 @@ const OneEvent: React.FC<OnBeacoinProp> = ({ id_beacoin }) => {
 
   return (
     <>
-      <div className=" flex flex-col justify-center items-center gap-3 px-7 py-5 hover:ring-2 hover:ring-[--primary] rounded-xl duration-100 cursor-pointer">
+      <Link
+        href="/Payment"
+        className=" flex flex-col justify-center items-center gap-3 px-7 py-5 hover:ring-2 hover:ring-[--primary] rounded-xl duration-100 cursor-pointer"
+      >
         <Image
           src={image.getUrl()}
           width={100}
@@ -28,7 +31,7 @@ const OneEvent: React.FC<OnBeacoinProp> = ({ id_beacoin }) => {
           <p>{beacoin.getNumber()} beacoins </p>
           <p className=" opacity-50">{beacoin.getPrice()} €</p>
         </div>
-      </div>
+      </Link>
     </>
   );
 };
