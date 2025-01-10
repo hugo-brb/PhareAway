81671b937007222a0094eb24b79c8849f6cc7758
sinfixx <sinfix069@gmail.com>
2025-01-10 11:43:55 +0100

    feat: Hint Button + big update Enigma

--- components/popover/JohnPork.tsx
+++ components/popover/JohnPork.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { text } from "stream/consumers";
-import React from "react";
 import Image from "next/image";
 
 interface SmallEnigmeProps {
@@ -28,7 +26,7 @@ export default function SmallEnigme({
 }: SmallEnigmeProps) {
   return (
     <section
-      style={{ left: `${coordX}vw`, top: `${coordY - 2}vw` }}
+      style={{ left: `44vw`, top: `25vw` }}
       className={`absolute w-[20vw] h-[10vw] bg-white bg-opacity-60 rounded-3xl backdrop-blur-md px-7 py-12 overflow-y-scroll scrollbarhidden`}
     >
       <div className="flex flex-col items-center">
@@ -38,7 +36,7 @@ export default function SmallEnigme({
             className="absolute top-5 left-5 transform transition-transform duration-300 hover:-rotate-90"
             onClick={() => handleClickPopup("0")} //fonction permettant de fermer la fenetre et revenir à l'état de base de l'élément parent
           >
-            <img
+            <Image
               src="/icones/xmark-solid.svg"
               alt="arrow-back"
               width={24}
@@ -56,48 +54,6 @@ export default function SmallEnigme({
           />
           <span>{text2}</span>
         </div>
-      </div>
-    </section>
-  );
-}
-
-export default function SmallEnigme({
-  handleClickPopup,
-  codeLock,
-  coordX,
-  coordY,
-  name,
-  question,
-  text1,
-  answer,
-  text2,
-}: SmallEnigmeProps) {
-  return (
-    <section
-      style={{ left: `${coordX}vw`, top: `${coordY - 2}vw` }}
-      className={`absolute w-[20vw] h-[10vw] bg-white bg-opacity-60 rounded-3xl backdrop-blur-md px-7 py-12 overflow-y-scroll scrollbarhidden`}
-    >
-      <button
-        className="absolute top-5 left-5 transform transition-transform duration-300 hover:-rotate-90"
-        onClick={() => handleClickPopup("0")} //fonction permettant de fermer la fenetre et revenir à l'état de base de l'élément parent
-      >
-        <Image
-          src="/icones/xmark-solid.svg"
-          alt="arrow-back"
-          width={24}
-          height={24}
-        />
-      </button>
-      <div className="flex flex-col items-center">
-        <h1>{name}</h1>
-        <span>{question}</span>
-        <span>{text1}</span>
-        <input
-          className="flex items-center"
-          type="text"
-          placeholder="entrez la réponse ici"
-        />
-        <span>{text2}</span>
       </div>
     </section>
   );
