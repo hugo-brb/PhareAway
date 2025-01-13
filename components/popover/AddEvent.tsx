e183d3ce8c84a1c465e6683522625cb4c46d1238
Nereol <pimboliwell.fr@gmail.com>
2025-01-13 09:15:21 +0100

    croix à droite

--- components/popover/AddEvent.tsx
+++ components/popover/AddEvent.tsx
@@ -146,12 +146,12 @@ export default function Event({ handleClickActive }: MenuProps) {
     <>
       <main className=" absolute top-0 z-40 flex  w-[100vw] h-[100vh]">
         <section className=" flex flex-col self-center gap-12 w-[75vw] h-[95vh] bg-white bg-opacity-60 rounded-3xl backdrop-blur-md mx-auto px-7 py-12 overflow-y-scroll scrollbarhidden">
-          <button
-            className="absolute top-5 left-5 duration-300 ease-in-out hover:scale-110"
-            onClick={() => handleClickActive("calendar")}
+        <button
+            className="absolute top-5 right-5 transform transition-transform duration-300 hover:rotate-90"
+            onClick={() => handleClickActive("home")}
           >
             <Image
-              src="/icones/arrow-back.svg"
+              src="/icones/xmark-solid.svg"
               alt="arrow-back"
               width={24}
               height={24}
