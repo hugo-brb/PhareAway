25e8236ca8463b6269121ca53ced163716e18d14
Argell <flxflx@outlook.fr>
2025-01-14 15:34:01 +0100

    feat: début de l'implémentation du bouton trie, à finir de mettre en lien les boutons avec la requête sql

--- components/popover/SearchBar.tsx
+++ components/popover/SearchBar.tsx
@@ -3,11 +3,13 @@ import React, { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 import OneEvent from "@/components/OneEvent";
 import Image from "next/image"; // Image du bouton "loop"
+import Button from "./SortButton";
 
 const supabaseData =createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
   );
+
 
 
 const SearchBar = () => {
@@ -20,6 +22,8 @@ const SearchBar = () => {
   const [results, setResults] = useState<Event[]>([]);
   const [loading, setLoading] = useState(true);
 
+
+  
   useEffect(() => {
     const fetchData = async () => {
       const currentDate = new Date().toISOString();
@@ -73,16 +77,22 @@ const SearchBar = () => {
             className="flex mb-10 md:mt-0 items-center self-center gap-4"
             >
             {/*Bouton trier */}
-            <button className=" flex items-center gap-2 bg-[--primary] ring-2 ring-[--primary] rounded-2xl duration-500 hover:bg-transparent w-fit self-center py-2 px-3 text-base">
-              <svg
-                className=" size-3 fill-[--text]"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-              >
+            <div>
+              <button className=" flex items-center gap-2 bg-[--primary] ring-2 ring-[--primary] rounded-2xl duration-500 hover:bg-transparent w-fit self-center py-2 px-3 text-base">
+                <svg
+                  className=" size-3 fill-[--text]"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
                 <path d="M3.9 54.9C10.5 40.9 24.5 32 40 32l432 0c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9 320 448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6l0-79.1L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z" />
-              </svg>
-              <span>Trier</span>
-            </button>
+                </svg>
+                <span>Trier</span>
+                </button>
+
+            </div>
+
+            <Button />
+
             {/*Fin Bouton trier */}
             {/* Search Bar */}
             <div className="relative">
