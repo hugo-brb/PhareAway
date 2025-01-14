045a8e0fa0f2bbda3e4fa922a3db34d1c7020da8
Nereol <pimboliwell.fr@gmail.com>
2025-01-14 16:25:51 +0100

    add: ajout de l'achat des dlc

--- components/OneExtention.tsx
+++ components/OneExtention.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Image from "next/image";
 import { useExtention } from "./model/extention";
+import { UsePlayer } from "./model/player";
 
 interface OnExtentionProp {
   id_extention: number;
+  player:UsePlayer;
 }
 
-const OneEvent: React.FC<OnExtentionProp> = ({ id_extention }) => {
+const mapExtention: { [key: number]: string } = {1:"Corse",2:"Guadeloupe",3:"Guyane",4:"Martinique",5:"Reunion",6:"Mayotte"};
+const OneEvent: React.FC<OnExtentionProp> = ({ id_extention, player }) => {
   //création d'un instance de la classe extention
   const extention = useExtention(id_extention);
   //Récupération de l'image de associé a l'offre de extention
@@ -14,12 +17,51 @@ const OneEvent: React.FC<OnExtentionProp> = ({ id_extention }) => {
 
   // si l'offre de l'extention n'a pas encore charger affiché un message de chargement
   if (!extention) {
-    return <p>Chargement de l&apos;événement...</p>;
+    return <p>Chargement de l&apos;éxtention...</p>;
   }
 
   return (
     <>
-      <div className=" flex flex-col justify-center items-center gap-3 px-7 py-5 hover:ring-2 hover:ring-[--primary] rounded-xl duration-100 cursor-pointer">
+      <div className=" flex flex-col justify-center items-center gap-3 px-7 py-5 hover:ring-2 hover:ring-[--primary] rounded-xl duration-100 cursor-pointer"
+       onClick={() => {
+        const dlc = player.getDlcUnlocked();
+        if (id_extention!=3){//3
+            if (dlc[id_extention] === 1) {
+            alert("Vous avez déjà débloqué cette extension");
+            }
+            else{
+                if (player.getBeacoins() >= 200) {
+                    player.setBeacoins(-200);
+                    dlc[id_extention] = 1;
+                    player.setDlcUnlocked(dlc);
+                    alert("Vous avez débloqué l'extensions : "+mapExtention[id_extention]);
+                }
+                else {
+                    alert(
+                        "Vous n'avez pas assez de beacoins pour acheter cette extension"
+                    );
+                }
+            }}
+        else {
+            if (dlc.every((value: number) => value === 1)) {
+                alert("Vous avez déjà débloqué cette extension");
+                }
+                else{
+                    if (player.getBeacoins() >= 800) {
+                        player.setBeacoins(-800);
+                        player.setDlcUnlocked([1,1,1,1,1,1,1]);
+                        alert("Vous avez débloqué toutes les extensions");
+                    }
+                    else {
+                        alert(
+                            "Vous n'avez pas assez de beacoins pour acheter cette extension"
+                        );
+                    }
+                }}
+
+            }
+        }
+      >
         {/*Affichage de l'image de l'offre d'extention si elle existe sinon affiché un place holder */}
         {image.getUrl() !== "" ? (
           <Image
