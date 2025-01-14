25106dd80beb98a14b880d96620ebc6d2870ec8e
sinfixx <sinfix069@gmail.com>
2025-01-14 11:28:38 +0100

    feat: night enigma

--- components/popover/JohnPork.tsx
+++ components/popover/JohnPork.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { useState } from "react";
 import AnswerPop from "@/components/popover/Answer";
-import { usePlayer } from "../model/player";
+import { UsePlayer } from "../model/player";
 
 interface SmallEnigmeProps {
   handleClickPopup: (a: string) => void;
@@ -122,6 +122,17 @@ export default function SmallEnigme({
 ⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄
 Ajout de 2000 beacoins sur le compte de ${player.getPrenom()}`);
               player.setBeacoins(2000);
+            } else if (lh === 104 && id === "3") {
+              const currentHour = new Date().getHours(); // Récupère l'heure actuelle (0-23)
+
+              if (
+                (currentHour >= 18 && currentHour <= 23) ||
+                (currentHour >= 0 && currentHour < 6)
+              ) {
+                handleClickAnswer("Vrai");
+              } else {
+                handleClickAnswer("Faux");
+              }
             } else {
               handleClickAnswer("Faux");
             }
