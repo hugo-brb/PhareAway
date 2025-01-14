910030088e9d1738458ed8d49649a87614c5b89a
barbiehu <hugo.barbieri@etu.univ-grenoble-alpes.fr>
2025-01-14 11:27:56 +0100

    chore: update Enigme component

--- components/popover/Enigme.tsx
+++ components/popover/Enigme.tsx
@@ -107,7 +107,7 @@ export default function Enigme({
           </button>
 
           <div
-            className="rounded-3xl absolute top-1/2 -translate-y-1/2 md:left-[10%] w-[80vw] h-[80vw] bg-cover bg-center"
+            className="rounded-3xl absolute top-1/2 -translate-y-1/2 md:left-[10%] w-[80vw] h-[80vw] md:w-[40vw] md:h-[80vh] bg-cover bg-center"
             style={{
               backgroundImage: `url("https://nereoll.github.io/imagesPhare/phares/${id}.png")`,
             }}
