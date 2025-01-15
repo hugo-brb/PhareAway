19693630a0960900fa4ceeb2cfcd0b8db6f120db
Argell <flxflx@outlook.fr>
2025-01-15 17:22:47 +0100

    change: changement de l'icone de sort (a redimensionner)

--- components/popover/SearchBar.tsx
+++ components/popover/SearchBar.tsx
@@ -4,6 +4,7 @@ import OneEvent from "@/components/OneEvent";
 import Image from "next/image"; // Image du bouton "loop"
 import FilterButtons from "./SortButton";
 
+
 const supabaseData = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!
@@ -115,10 +116,16 @@ const SearchBar = () => {
             className="flex items-center gap-2 bg-[--primary] ring-2 ring-[--primary] rounded-2xl duration-500 hover:bg-transparent w-fit self-center py-2 px-3 text-base"
           >
             <svg
-              className="size-3 fill-[--text]"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
             >
+          <Image
+            width={24}
+            height={24}
+            src="../icones/loop.svg"
+            alt="Sort Icon"
+
+            />
+
               <path d="M3.9 54.9C10.5 40.9 24.5 32 40 32l432 0c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9 320 448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6l0-79.1L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z" />
             </svg>
             <span>Trier</span>
