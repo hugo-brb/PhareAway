36be68a70fbfbb21284b7cace0d08d7cff1da257
bastien <bastien.allegre@etu.univ-grenoble-alpes.fr>
2025-01-16 08:53:09 +0100

    feat : replace element in nav bar

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -27,20 +27,20 @@ export default function Menu({ active, handleClickActive }: MenuProps) {
         <hr className=" hidden md:block" />
         <div
           className="flex flex-col justify-center items-center gap-4"
-          onClick={() => handleClickActive("picture")}
+          onClick={() => handleClickActive("coin")}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 576 512"
+            viewBox="0 0 512 512"
             className={` ${
-              active === "picture"
+              active === "coin"
                 ? "fill-[--accent]"
                 : "fill-[--primary] hover:fill-[--accent]"
             } duration-300 size-12 cursor-pointer`}
           >
-            <path d="M160 32c-35.3 0-64 28.7-64 64l0 224c0 35.3 28.7 64 64 64l352 0c35.3 0 64-28.7 64-64l0-224c0-35.3-28.7-64-64-64L160 32zM396 138.7l96 144c4.9 7.4 5.4 16.8 1.2 24.6S480.9 320 472 320l-144 0-48 0-80 0c-9.2 0-17.6-5.3-21.6-13.6s-2.9-18.2 2.9-25.4l64-80c4.6-5.7 11.4-9 18.7-9s14.2 3.3 18.7 9l17.3 21.6 56-84C360.5 132 368 128 376 128s15.5 4 20 10.7zM192 128a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM48 120c0-13.3-10.7-24-24-24S0 106.7 0 120L0 344c0 75.1 60.9 136 136 136l320 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-320 0c-48.6 0-88-39.4-88-88l0-224z" />
+            <path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64l0 48-128 0 0-48zm-48 48l-64 0c-26.5 0-48 21.5-48 48L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-208c0-26.5-21.5-48-48-48l-64 0 0-48C336 50.1 285.9 0 224 0S112 50.1 112 112l0 48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z" />
           </svg>
-          <p className="hidden md:flex justify-center text-xs">Images</p>
+          <p className="hidden md:flex justify-center text-xs">PharAchat</p>
         </div>
         <hr className=" hidden md:block" />
         <div
@@ -61,22 +61,23 @@ export default function Menu({ active, handleClickActive }: MenuProps) {
           <p className="hidden md:flex justify-center text-xs">Carte</p>
         </div>
         <hr className=" hidden md:block" />
+        {/*=================================*/}
         <div
           className="flex flex-col justify-center items-center gap-4"
-          onClick={() => handleClickActive("coin")}
+          onClick={() => handleClickActive("picture")}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
+            viewBox="0 0 576 512"
             className={` ${
-              active === "coin"
+              active === "picture"
                 ? "fill-[--accent]"
                 : "fill-[--primary] hover:fill-[--accent]"
             } duration-300 size-12 cursor-pointer`}
           >
-            <path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64l0 48-128 0 0-48zm-48 48l-64 0c-26.5 0-48 21.5-48 48L0 416c0 53 43 96 96 96l256 0c53 0 96-43 96-96l0-208c0-26.5-21.5-48-48-48l-64 0 0-48C336 50.1 285.9 0 224 0S112 50.1 112 112l0 48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z" />
+            <path d="M160 32c-35.3 0-64 28.7-64 64l0 224c0 35.3 28.7 64 64 64l352 0c35.3 0 64-28.7 64-64l0-224c0-35.3-28.7-64-64-64L160 32zM396 138.7l96 144c4.9 7.4 5.4 16.8 1.2 24.6S480.9 320 472 320l-144 0-48 0-80 0c-9.2 0-17.6-5.3-21.6-13.6s-2.9-18.2 2.9-25.4l64-80c4.6-5.7 11.4-9 18.7-9s14.2 3.3 18.7 9l17.3 21.6 56-84C360.5 132 368 128 376 128s15.5 4 20 10.7zM192 128a32 32 0 1 1 64 0 32 32 0 1 1 -64 0zM48 120c0-13.3-10.7-24-24-24S0 106.7 0 120L0 344c0 75.1 60.9 136 136 136l320 0c13.3 0 24-10.7 24-24s-10.7-24-24-24l-320 0c-48.6 0-88-39.4-88-88l0-224z" />
           </svg>
-          <p className="hidden md:flex justify-center text-xs">PharAchat</p>
+          <p className="hidden md:flex justify-center text-xs">Images</p>
         </div>
         <hr className=" hidden md:block" />
         <div
